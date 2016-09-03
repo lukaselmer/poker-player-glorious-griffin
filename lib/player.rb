d590cc2d3d3e2447983f18aa3770e810450149a0
Lukas Elmer <lukas.elmer@renuo.ch>
2016-09-03 11:01:55 +0200

    change version

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   GIT_RELEASE = `git log --pretty=format:\"%h\" -1`.freeze
-  VERSION = "Two Griffins #{Time.now} #{GIT_RELEASE} #{ENV}".freeze
+  VERSION = "Two Griffins #{Time.now} #{GIT_RELEASE}".freeze
 
   def bet_request(_game_state)
     0
