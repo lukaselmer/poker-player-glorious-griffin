feab509195f1fc8afbb1fa98fe40f1cdbfb1d7f0
Lukas Elmer <lukas.elmer@renuo.ch>
2016-09-03 11:10:54 +0200

    allow get requests

--- player_service.rb
+++ player_service.rb
@@ -17,3 +17,16 @@ post '/' do
     'OK'
   end
 end
+
+get '/' do
+  if params[:action] == 'bet_request'
+    Player.new.bet_request(JSON.parse(params[:game_state])).to_s
+  elsif params[:action] == 'showdown'
+    Player.new.showdown(JSON.parse(params[:game_state]))
+    'OK'
+  elsif params[:action] == 'version'
+    Player::VERSION
+  else
+    'OK'
+  end
+end
